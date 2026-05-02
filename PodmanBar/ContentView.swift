10ffd53130cbd2d41aed09f101749e8b1a5896ae
singgih <singgihnn@gmail.com>
2026-05-02 09:32:03 +0700

    Initial Commit

---
+++ PodmanBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PodmanBar
+//
+//  Created by Lit on 02/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PodmanBar/PodmanBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodmanBarApp.swift
+//  PodmanBar
+//
+//  Created by Lit on 02/05/26.
+//
+
+import SwiftUI
+
+@main
+struct PodmanBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
